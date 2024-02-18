5c18ac7a683e42017025d72381a89c45bc9c41ba
Leo olofsson <leo@letnh.com>
2024-02-18 20:54:55 +0100

    Added no_ns and all domaned manage by hetzner

--- dnsconfig.js
+++ dnsconfig.js
@@ -1,6 +1,21 @@
-var REG_NONE = NewRegistrar("none");
+var REG_NONE = NewRegistrar("none", {
+  no_ns: "true",
+});
 var DSP_HETZNER = NewDnsProvider("hetzner");
 
-D("letnh.com", REG_NONE, DnsProvider(DSP_HETZNER),
-    A("@", "1.2.3.4")
+DEFAULTS(DefaultTTL("1h"), NAMESERVER_TTL("30m"))
+
+D("letnh.com", REG_NONE, DnsProvider(DSP_HETZNER)
+);
+
+D("hamsterapps.net", REG_NONE, DnsProvider(DSP_HETZNER)
+);
+
+D("letnh.dev", REG_NONE, DnsProvider(DSP_HETZNER)
+);
+
+D("letnh.xyz", REG_NONE, DnsProvider(DSP_HETZNER)
+);
+
+D("nerdyhamster.net", REG_NONE, DnsProvider(DSP_HETZNER)
 );
