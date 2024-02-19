be167da90895df0f37873169d4a32b90e3eb9d58
Leo olofsson <leo@letnh.com>
2024-02-19 21:00:18 +0100

    Added SPF records

--- dnsconfig.js
+++ dnsconfig.js
@@ -3,21 +3,35 @@ var REG_NONE = NewRegistrar("none", {
 });
 var DSP_HETZNER = NewDnsProvider("hetzner");
 
+var SPF = SPF_BUILDER({
+  label: "@",
+  overflow: "_spf%d",
+  raw: "_rawspf",
+  ttl: "10m",
+  parts: [
+    "v=spf1",
+    "include:spf.messagingengine.com",
+    "-all"
+  ]
+});
+
 DEFAULTS(DefaultTTL("1h"), NAMESERVER_TTL("30m"))
 
 D("letnh.com", REG_NONE, DnsProvider(DSP_HETZNER),
-  A("@", "1.1.1.1")
+  SPF
 );
 
 D("hamsterapps.net", REG_NONE, DnsProvider(DSP_HETZNER),
-  A("@", "1.1.1.1")
+  SPF
 );
 
 D("letnh.dev", REG_NONE, DnsProvider(DSP_HETZNER)
 );
 
-D("letnh.xyz", REG_NONE, DnsProvider(DSP_HETZNER)
+D("letnh.xyz", REG_NONE, DnsProvider(DSP_HETZNER),
+  SPF
 );
 
-D("nerdyhamster.net", REG_NONE, DnsProvider(DSP_HETZNER)
+D("nerdyhamster.net", REG_NONE, DnsProvider(DSP_HETZNER),
+  SPF
 );
